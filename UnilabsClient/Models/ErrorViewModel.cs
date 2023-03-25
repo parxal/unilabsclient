92ed92c467d99d00e66181aee2ab87163b600386
Ivan Valadares <ivanv@dxspark.com>
2023-03-25 01:45:04 +0000

    first

---
+++ UnilabsClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UnilabsClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
